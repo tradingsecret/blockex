c26d2975b4a95bde67abc60f20793e0d4b7b2f8b
Developer1 <developer1@gmail.com>
2021-09-07 05:30:20 +0100

    Updated wsgi

--- blockex/wsgi.py
+++ blockex/wsgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blockex.settings')
+wsgi_app = get_wsgi_application()
 
-application = get_wsgi_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from explorer.consumers import ChatConsumer
+
+application = ProtocolTypeRouter({
+    "http": wsgi_app,
+    # WebSocket chat handler
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            url(r'ws/explorer/', ChatConsumer.as_asgi()),
+        ])
+    ),
+})
