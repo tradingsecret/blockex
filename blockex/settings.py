6e304d3216ac838a9f55266aef7d0201ba5ad2d0
Developer1 <developer1@gmail.com>
2021-09-19 13:36:38 +0100

    Updated apps

--- blockex/settings.py
+++ blockex/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blockex.middleware.corsMiddleware',
 ]
 
 ROOT_URLCONF = 'blockex.urls'
